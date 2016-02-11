438d0e7c7080db962427dd2971ea5b07d8799634
Vance McCorkle <thorskettle@hotmail.com>
2016-02-11 12:34:11 -0800

    add

---
+++ OdbcWidget/OdbcWidget.cs
@@ -0,0 +1,172 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Odbc;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+
+namespace DataTools
+{
+    public class OdbcWidget
+    {
+        public const string RegObdcSubKey = @"Software\ODBC\ODBC.INI\ODBC Data Sources";
+
+        public static string[] ListDataSourceNames()
+        {
+            List<string> resultsLocalMachine = ListDataSourceNames(Registry.LocalMachine).ToList();
+
+            List<string> resultsCurrentUser = ListDataSourceNames(Registry.CurrentUser).ToList();
+
+            List<string> combinedResults = new List<string>();
+
+            combinedResults.AddRange(resultsLocalMachine);
+
+            combinedResults.AddRange(resultsCurrentUser);
+
+            return combinedResults.ToArray();
+        }
+
+        public static string[] ListDataSourceNames(RegistryKey rootKey)
+        {
+            RegistryKey regKey = rootKey.OpenSubKey(RegObdcSubKey);
+
+            string[] retVal = null;
+
+            if (regKey != null)
+            {
+                retVal = regKey.GetValueNames();
+            }
+
+            return retVal;
+        }
+
+        public static string CreateConnectionString(string odbcDsn, string uid, string pwd, string additionalParameters="")
+        {
+            const string odbcConnectionTemplate =
+                "uid={0};pwd={1};{2}";
+
+            string retVal = CreateConnectionString(odbcDsn, string.Format(odbcConnectionTemplate, uid, pwd, additionalParameters));
+
+            return retVal;
+        }
+
+        public static string CreateConnectionString(string odbcDsn, string additionalParameters="")
+        {
+            const string odbcConnectionTemplate =
+                "DSN={0};{1}";
+
+            string retVal = string.Format(odbcConnectionTemplate, odbcDsn, additionalParameters);
+
+            return retVal;
+        }
+
+        public static OdbcConnection CreateObdcConnection(string odbcDsn)
+        {
+            string connStr = CreateConnectionString(odbcDsn);
+
+            OdbcConnection connection = new OdbcConnection(connStr);
+
+            connection.Open();
+
+            return connection;
+        }
+
+        private static bool InternalTestConnection(string connectionStr)
+        {
+            bool testConnection;
+
+            using (OdbcConnection connection = new OdbcConnection(connectionStr))
+            {
+                connection.Open();
+
+                testConnection = (connection.State == ConnectionState.Open);
+            }
+
+            return testConnection;
+        }
+
+        //private static bool InternalTestConnection(OdbcConnection connection)
+        //{
+        //    bool testConnection = false;
+
+        //    if (connection != null)
+        //    {
+        //        connection.Open();
+
+        //        testConnection = (connection.State == ConnectionState.Open);
+        //    }
+
+        //    return testConnection;
+        //}
+
+        public static bool TestConnection(string connectionStr, int timeoutInMilliseconds = 10000)
+        {
+            Func<string, bool> testConnectionDelegate = InternalTestConnection;
+
+            var testConnection = ImplicitWaitUntilTimeout(testConnectionDelegate, connectionStr, timeoutInMilliseconds);
+
+            return testConnection;
+        }
+
+        //public static bool TestConnection(OdbcConnection connection, int timeoutInMilliseconds = 10000)
+        //{
+        //    Func<string, bool> testConnectionDelegate = InternalTestConnection;
+
+        //    var testConnection = ImplicitWaitUntilTimeout(testConnectionDelegate, connection, timeoutInMilliseconds);
+
+        //    return testConnection;
+        //}
+
+        public object ExecuteScalar(string queryString, string dpiInstanceName = "DPI_TEST")
+        {
+            var command = new OdbcCommand(queryString);
+
+            var odbcConn = CreateObdcConnection(dpiInstanceName);
+
+            command.Connection = odbcConn;
+
+            var result = command.ExecuteScalar();
+
+            return result;
+        }
+
+        public OdbcDataReader ExecuteReader(string queryString, string dsn)
+        {
+            var command = new OdbcCommand(queryString);
+
+            var odbcConn = CreateObdcConnection(dsn);
+       
+            command.Connection = odbcConn;
+
+            var result = command.ExecuteReader();
+
+            return result;
+        }
+
+        private static bool ImplicitWaitUntilTimeout<TParamType>(Func<TParamType, bool> methodDelegate, TParamType param, int timeOutInMilliseconds)
+        {
+            bool result = false;
+            var timer = new Stopwatch();
+
+            timer.Start();
+
+            do
+            {
+                try
+                {
+                    result = methodDelegate(param);
+
+                }
+                catch (Exception)
+                {
+                    Thread.Sleep(100);
+                }
+
+            } while (result == false && (timer.ElapsedMilliseconds < timeOutInMilliseconds));
+
+            return result;
+        }
+    }
+}
---
+++ OdbcWidgetTests/OdbcWidgetTests.cs
@@ -0,0 +1,89 @@
+﻿using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.Win32;
+
+namespace OdbcWidgetTests
+{
+    using DataTools;
+
+    [TestClass]
+    public class OdbcWidgetTests
+    {
+        [TestMethod]
+        public void GetOdbcConnectionsCurrentUser()
+        {
+            var result = OdbcWidget.ListDataSourceNames(Registry.CurrentUser);
+
+            Assert.IsTrue(result.ToArray().Length > 5);
+        }
+
+        [TestMethod]
+        public void GetOdbcConnectionsLocalMachine()
+        {
+            var result = OdbcWidget.ListDataSourceNames(Registry.LocalMachine);
+
+            Assert.IsTrue(result.ToArray().Length > 0);
+        }
+
+        [TestMethod]
+        public void GetOdbcConnectionsAll()
+        {
+            var result = OdbcWidget.ListDataSourceNames();
+
+            Assert.IsTrue(result.ToArray().Length > 6);
+        }
+
+        [TestMethod]
+        public void CreateConnectionStrings()
+        {
+            var results = OdbcWidget.ListDataSourceNames();
+
+            foreach (var dsn in results)
+            {
+                var result = OdbcWidget.CreateConnectionString(dsn, "Satch", "Guitar+1", "DataCompression=True");
+
+                Assert.AreEqual(result,
+                    string.Format("DSN={0};uid=Satch;pwd=Guitar+1;DataCompression=True", dsn));
+            }
+
+            foreach (var dsn in results)
+            {
+                var result = OdbcWidget.CreateConnectionString(dsn, "Satch", "Guitar+1");
+
+                Assert.AreEqual(result,
+                    string.Format("DSN={0};uid=Satch;pwd=Guitar+1;", dsn));
+            }
+
+            foreach (var dsn in results)
+            {
+                var result = OdbcWidget.CreateConnectionString(dsn);
+
+                Assert.AreEqual(result,
+                    string.Format("DSN={0};", dsn));
+            }
+        }
+
+        [TestMethod]
+        public void TestConnection()
+        {
+
+            var validConnStr = OdbcWidget.CreateConnectionString("DPI_Test", "S_RSETATST", "y3nsrrhb1", "DataCompression=True");
+
+            bool result = OdbcWidget.TestConnection(validConnStr);
+
+            Assert.IsTrue(result);
+
+            validConnStr = OdbcWidget.CreateConnectionString("Triad");
+
+            result = OdbcWidget.TestConnection(validConnStr);
+
+            Assert.IsTrue(result);
+
+            var invalidConnStr = OdbcWidget.CreateConnectionString("bogusDsn", "foo", "bar");
+
+            result = OdbcWidget.TestConnection(invalidConnStr, 2);
+
+            Assert.IsFalse(result);
+        }
+    }
+}
